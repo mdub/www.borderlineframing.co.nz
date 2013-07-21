b6d5f06e391b00fac87101af109c9b99b11f026f
Mike Williams <mdub@dogbiscuit.org>
2013-07-21 15:48:09 +1000

    Add testimonials page.

--- src/_pith/config.rb
+++ src/_pith/config.rb
@@ -1,2 +1,10 @@
 # project.assume_content_negotiation = true
 # project.assume_directory_index = true
+
+project.helpers do
+
+  def quote(from, &block)
+    include("/_partials/quote.html.haml", from: from, &block)
+  end
+
+end
